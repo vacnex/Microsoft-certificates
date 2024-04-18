a570cd598d86412a038c26927daa6fc4401f83d3
trần kính hoàng <92006941+hoaug-tran@users.noreply.github.com>
2024-04-19 00:57:39 +0700

    Update sign.sh

--- sign.sh
+++ sign.sh
@@ -1,11 +1,8 @@
 #!/bin/bash
 
 sudo apt update && sudo apt upgrade
-
 sudo apt-get install unzip
-
 sudo apt-get install sbsigntool
-
 sudo apt-get install efitools
 
 git clone https://github.com/hoaug-tran/Microsoft-certificates.git
@@ -13,22 +10,16 @@ git clone https://github.com/hoaug-tran/Microsoft-certificates.git
 mkdir ~/efikeys
 cd efikeys
 
-random_chars=$(head /dev/urandom | tr -dc A-Za-z0-9 | head -c 4)
-
+random_chars=$(head /dev/urandom | tr -dc A-Za-z0-9 | head -c 6)
 openssl req -new -x509 -newkey rsa:2048 -sha256 -days 3650 -nodes -subj "/CN=$random_chars PK Platform Key/" -keyout PK.key -out PK.pem
-
 openssl req -new -x509 -newkey rsa:2048 -sha256 -days 3650 -nodes -subj "/CN=$random_chars KEK Exchange Key/" -keyout KEK.key -out KEK.pem
-
 openssl req -new -x509 -newkey rsa:2048 -sha256 -days 3650 -nodes -subj "/CN=$random_chars ISK Image Signing Key/" -keyout ISK.key -out ISK.pem
-
 chmod 0600 *.key
 
 cp /home/$USER/Microsoft-certificates/MicCorUEFCA2011_2011-06-27.crt ~/efikeys/
-
 cp /home/$USER/Microsoft-certificates/MicWinProPCA2011_2011-10-19.crt ~/efikeys/
 
 openssl x509 -in MicWinProPCA2011_2011-10-19.crt -inform DER -out MicWinProPCA2011_2011-10-19.pem -outform PEM
-
 openssl x509 -in MicCorUEFCA2011_2011-06-27.crt -inform DER -out MicCorUEFCA2011_2011-06-27.pem -outform PEM
 
 cert-to-efi-sig-list -g $(uuidgen) PK.pem PK.esl
@@ -40,13 +31,10 @@ cert-to-efi-sig-list -g $(uuidgen) MicCorUEFCA2011_2011-06-27.pem MicCorUEFCA201
 cat ISK.esl MicWinProPCA2011_2011-10-19.esl MicCorUEFCA2011_2011-06-27.esl > db.esl
 
 sign-efi-sig-list -k PK.key -c PK.pem PK PK.esl PK.auth
-
 sign-efi-sig-list -k PK.key -c PK.pem KEK KEK.esl KEK.auth
-
 sign-efi-sig-list -k KEK.key -c KEK.pem db db.esl db.auth
 
 mkdir oc
-
 cp ISK.key ISK.pem oc
 cd oc
 
@@ -56,7 +44,6 @@ _get_opencore_url() {
     echo "$url"
 }
 
-# Function to get the latest URL of Opencore-Mod release
 _get_opencore_mod_url() {
     local urlsource=$(curl -s "https://api.github.com/repos/wjz304/OpenCore_NO_ACPI_Build/releases/latest" | grep "browser_download_url")
     local url=$(echo "$urlsource" | grep "RELEASE.zip" | head -n 1 | cut -d '"' -f 4)
